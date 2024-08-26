9de67fc8b01541164d55436bbfe517e5ad8087dc
DeedleFake <deedlefake@users.noreply.github.com>
2024-08-26 18:09:03 -0400

    cmd/playbot: show deferred response to give more time

---
+++ cmd/playbot/handle.go
@@ -0,0 +1,84 @@
+package main
+
+import (
+	"log/slog"
+	"strings"
+
+	"deedles.dev/xiter"
+	"github.com/DeedleFake/go-playground-bot/internal/extract"
+	"github.com/DeedleFake/go-playground-bot/internal/play"
+	"github.com/bwmarrin/discordgo"
+)
+
+func handleBlock(dg *discordgo.Session, i *discordgo.Interaction, block extract.CodeBlock) {
+	err := setupResponse(dg, i)
+	if err != nil {
+		slog.Error("setup response", "err", err)
+		return
+	}
+
+	result, err := play.Run(block.Source)
+	if err != nil {
+		slog.Error("run code", "err", err)
+		return
+	}
+	events := xiter.SliceChunksFunc(result.Events, func(ev play.Event) string { return ev.Kind })
+
+	var output strings.Builder
+	for chunk := range events {
+		output.WriteString(chunk[0].Kind)
+		output.WriteString(":\n```\n")
+		for _, ev := range chunk {
+			output.WriteString(ev.Message)
+		}
+		output.WriteString("\n```")
+	}
+
+	outputstr := output.String()
+	_, err = dg.InteractionResponseEdit(i, &discordgo.WebhookEdit{
+		Content: &outputstr,
+	})
+	if err != nil {
+		slog.Error("respond to interaction", "err", err)
+		return
+	}
+}
+
+func handleMessage(dg *discordgo.Session, i *discordgo.Interaction, msg *discordgo.Message) {
+	var found bool
+	for block := range extract.CodeBlocks(msg.Content) {
+		if block.Language != "go" && block.Language != "" {
+			continue
+		}
+
+		found = true
+		handleBlock(dg, i, block)
+	}
+	if !found {
+		err := dg.InteractionRespond(i, &discordgo.InteractionResponse{
+			Type: discordgo.InteractionResponseChannelMessageWithSource,
+			Data: &discordgo.InteractionResponseData{
+				Content: "No Go code blocks found. Note that all code blocks that are not either `go` blocks or have no language specified are ignored.",
+				Flags:   discordgo.MessageFlagsEphemeral,
+			},
+		})
+		if err != nil {
+			slog.Error("respond to interaction", "err", err)
+			return
+		}
+	}
+}
+
+func handleCommand(dg *discordgo.Session, i *discordgo.InteractionCreate) {
+	slog.Info("command", "name", i.ApplicationCommandData().Name, "user", i.Member.User)
+
+	data, ok := i.Data.(discordgo.ApplicationCommandInteractionData)
+	if !ok {
+		slog.Error("interaction data unexpected type", "data", i.Data)
+		return
+	}
+
+	for _, msg := range data.Resolved.Messages {
+		handleMessage(dg, i.Interaction, msg)
+	}
+}
--- cmd/playbot/playbot.go
+++ cmd/playbot/playbot.go
@@ -9,80 +9,17 @@ import (
 	"os/signal"
 	"strings"
 
-	"deedles.dev/xiter"
-	"github.com/DeedleFake/go-playground-bot/internal/extract"
-	"github.com/DeedleFake/go-playground-bot/internal/play"
 	"github.com/bwmarrin/discordgo"
 )
 
-func handleBlock(dg *discordgo.Session, i *discordgo.Interaction, block extract.CodeBlock) {
-	result, err := play.Run(block.Source)
-	if err != nil {
-		slog.Error("run code", "err", err)
-		return
-	}
-	events := xiter.SliceChunksFunc(result.Events, func(ev play.Event) string { return ev.Kind })
-
-	var output strings.Builder
-	for chunk := range events {
-		output.WriteString(chunk[0].Kind)
-		output.WriteString(":\n```\n")
-		for _, ev := range chunk {
-			output.WriteString(ev.Message)
-		}
-		output.WriteString("\n```")
-	}
-
-	err = dg.InteractionRespond(i, &discordgo.InteractionResponse{
-		Type: discordgo.InteractionResponseChannelMessageWithSource,
+func setupResponse(dg *discordgo.Session, i *discordgo.Interaction) error {
+	return dg.InteractionRespond(i, &discordgo.InteractionResponse{
+		Type: discordgo.InteractionResponseDeferredChannelMessageWithSource,
 		Data: &discordgo.InteractionResponseData{
-			Content: output.String(),
+			Content: "Working on it...",
 			Flags:   discordgo.MessageFlagsEphemeral,
 		},
 	})
-	if err != nil {
-		slog.Error("respond to interaction", "err", err)
-		return
-	}
-}
-
-func handleMessage(dg *discordgo.Session, i *discordgo.Interaction, msg *discordgo.Message) {
-	var found bool
-	for block := range extract.CodeBlocks(msg.Content) {
-		if block.Language != "go" && block.Language != "" {
-			continue
-		}
-
-		found = true
-		handleBlock(dg, i, block)
-	}
-	if !found {
-		err := dg.InteractionRespond(i, &discordgo.InteractionResponse{
-			Type: discordgo.InteractionResponseChannelMessageWithSource,
-			Data: &discordgo.InteractionResponseData{
-				Content: "No Go code blocks found. Note that all code blocks that are not either `go` blocks or have no language specified are ignored.",
-				Flags:   discordgo.MessageFlagsEphemeral,
-			},
-		})
-		if err != nil {
-			slog.Error("respond to interaction", "err", err)
-			return
-		}
-	}
-}
-
-func handleCommand(dg *discordgo.Session, i *discordgo.InteractionCreate) {
-	slog.Info("command", "name", i.ApplicationCommandData().Name, "user", i.Member.User)
-
-	data, ok := i.Data.(discordgo.ApplicationCommandInteractionData)
-	if !ok {
-		slog.Error("interaction data unexpected type", "data", i.Data)
-		return
-	}
-
-	for _, msg := range data.Resolved.Messages {
-		handleMessage(dg, i.Interaction, msg)
-	}
 }
 
 func commands() []*discordgo.ApplicationCommand {
